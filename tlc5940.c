90c69b5730db0370ab55778d2a62c69466767495
wingez <gustav.ledous@gmail.com>
2016-12-28 17:53:14 +0000

    Added file with code that directly handels the tlc5940 registers

---
+++ tlc5940.c
@@ -0,0 +1,161 @@
+
+
+#include "tlc5940.h"
+#include "config.h"
+#include <avr/interrupt.h>
+
+volatile  char needXLAT = 0;
+
+
+//unsigned int TLC5940::BrighnessValues[TLC5940_NUMREGISTERS * 16];
+
+const char /*TLC5940::*/xlatPins[4] = {
+	(1 << TLC5940_XLAT1),
+	(1 << TLC5940_XLAT2),
+	(1 << TLC5940_XLAT3),
+	(1 << TLC5940_XLAT4) };
+
+void TLC5940::Setup()
+{
+	cli();
+
+	TLC5940_VPRG_DDR |= (1 << TLC5940_VPRG);
+	TLC5940_SIN_DDR |= (1 << TLC5940_SIN);
+	TLC5940_SCLK_DDR |= (1 << TLC5940_SCLK);
+	TLC5940_BLANK_DDR |= (1 << TLC5940_BLANK);
+	TLC5940_GSCLK_DDR |= (1 << TLC5940_GSCLK);
+
+	TLC5940_BLANK_PORT |= (1 << TLC5940_BLANK);
+
+	char xlatcounter = TLC5940_NUMREGISTERS;
+	do
+	{
+		xlatcounter--;
+		TLC5940_XLAT_DDR |= (xlatPins[xlatcounter]);
+	} while (xlatcounter);
+
+
+	//TODOOOOOOOOOOOOOOOOOOOOO
+	TCCR2A = 0b00010010;
+	TCCR2B = 0b00000001;
+
+	TCCR1A = 0b00000000;
+	TCCR1B = 0b00001100;
+	TIMSK1 = 0b00000010;
+	OCR1A = 31;
+
+	sei();
+
+
+	SetDOT();
+
+	TLC5940_BLANK_PORT &= ~(1 << TLC5940_BLANK);
+
+	char counter = 0;
+	while (counter < TLC5940_NUMREGISTERS * 16)
+	{
+		BrightnessValues[counter] = 0;
+		counter++;
+	}
+
+	Update();
+
+
+
+
+}
+
+void TLC5940::Update()
+{
+	char chipCounter = 0;
+
+	char lampcounter;
+
+	unsigned int lampvalue;
+	unsigned int mask;
+
+	TLC5940_VPRG_PORT &= ~(1 << TLC5940_VPRG);
+	
+	while (chipCounter < TLC5940_NUMREGISTERS)
+	{
+		lampcounter = chipCounter * 16 + 15;
+		char lastLamp = chipCounter * 16;
+		while (1)
+		{
+			lampvalue = BrightnessValues[lampcounter];
+			mask = 0x800;
+			while (1)
+			{
+				if (lampvalue & mask)
+					TLC5940_SIN_PORT |= (1 << TLC5940_SIN);
+				else
+					TLC5940_SIN_PORT &= ~(1 << TLC5940_SIN);
+
+				TLC5940_SCLK_PORT |= (1 << TLC5940_SCLK);
+				TLC5940_SCLK_PORT &= ~(1 << TLC5940_SCLK);
+
+				mask = mask >> 1;
+
+				if (mask == 0)
+					break;
+			}
+
+			if (lampcounter == lastLamp)
+				break;
+			lampcounter--;
+
+		}
+		needXLAT = xlatPins[chipCounter];
+		while (needXLAT) {}
+
+		chipCounter++;
+	}
+
+}
+
+void TLC5940::SetDOT()
+{
+	TLC5940_VPRG_PORT |= (1 << TLC5940_VPRG);
+	TLC5940_SIN_PORT |= (1 << TLC5940_SIN);
+	char counter;
+	char chipcounter = 0;
+	while (chipcounter < TLC5940_NUMREGISTERS)
+	{
+		counter = 0;
+
+		while (counter < (16 * 6))
+		{
+			TLC5940_SCLK_PORT |= (1 << TLC5940_SCLK);
+			TLC5940_SCLK_PORT &= ~(1 << TLC5940_SCLK);
+			counter++;
+		}
+
+		needXLAT = xlatPins[chipcounter];
+		while (needXLAT) {}
+
+		chipcounter++;
+	}
+
+
+
+}
+
+
+
+ISR(TIMER1_OVF_vect) {}
+ISR(TIMER1_COMPB_vect) {}
+ISR(TIMER1_COMPA_vect)
+{
+	TLC5940_BLANK_PORT |= (1 << TLC5940_BLANK);
+
+	if (needXLAT)
+	{
+		TLC5940_XLAT_PORT |= needXLAT;
+		TLC5940_XLAT_PORT &= ~needXLAT;
+		TLC5940_SCLK_PORT |= (1 << TLC5940_SCLK);
+		TLC5940_SCLK_PORT &= ~(1 << TLC5940_SCLK);
+		needXLAT = 0;
+	}
+
+	TLC5940_BLANK_PORT &= ~(1 << TLC5940_BLANK);
+}
---
+++ tlc5940.h
@@ -0,0 +1,31 @@
+
+#ifndef TLC5940_H
+#define TLC5940_H
+
+#include "config.h"
+
+extern volatile char needXLAT;
+
+
+extern const char xlatPins[4];
+
+
+class TLC5940
+{
+public:
+	unsigned int BrightnessValues[TLC5940_NUMREGISTERS*16];
+
+	void Setup();
+	void Update();
+
+private:
+	void SetDOT();
+};
+
+
+
+
+
+#endif // !TLC5940_H
+
+
