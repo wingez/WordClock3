78ed0124ed5e168084fc7184f28455f412afcb41
wingez <gustav.ledous@gmail.com>
2016-12-30 20:30:10 +0000

    Better display-brightness values

--- gamma.c
+++ gamma.c
@@ -5,7 +5,7 @@
 
 const unsigned int Gamma::_gammab10s10[10] PROGMEM =
 {
-	500,1023,16,48,106,198,329,507,736,1023
+	50,100,150,220,329,480,620,736,880,1023
 };
 
 unsigned int Gamma::b10s10(unsigned char value)
