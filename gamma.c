e6214100caeaf0269f49a7a11b1857a59ccfc808
wingez <gustav.ledous@gmail.com>
2016-12-30 00:07:37 +0000

    Header and gamma-object added

---
+++ gamma.c
@@ -0,0 +1,14 @@
+
+#include <avr/pgmspace.h>
+#include "gamma.h"
+
+
+const unsigned int Gamma::_gammab10s10[10] PROGMEM =
+{
+	500,1023,16,48,106,198,329,507,736,1023
+};
+
+unsigned int Gamma::b10s10(unsigned char value)
+{
+	return pgm_read_dword(&(_gammab10s10[value]));
+}
--- gamma.h
+++ gamma.h
@@ -2,17 +2,16 @@
 #ifndef GAMMA_H
 #define GAMMA_H
 
-#include <avr/pgmspace.h>
 
-const unsigned int _gamma10b10s[10] PROGMEM =
+class Gamma
 {
-	1,3,16,48,106,198,329,507,736,1023
+public:
+	static unsigned int b10s10(unsigned char value);
+
+private:
+	static const unsigned int _gammab10s10[10];
 };
 
-unsigned int Gamma10b10s(unsigned char value)
-{
-	return pgm_read_dword(&(_gamma10b10s[value]));
-}
 
 
 
